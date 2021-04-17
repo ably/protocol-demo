fd17f31671358057e35e6da7546cd3d3b3e7569e
Bruce Thomas <burce.thomas@ably.com>
2021-04-17 12:21:57 +0100

    Added help instructions

---
+++ src/help.js
@@ -0,0 +1,31 @@
+module.exports = () => {
+    const ascii = `
+
+         ████
+        ██  ██                 ▄▄     ▄▄▄▄                    ▄▄▄▄     ██
+       ██    ██                ██      ▀██                    ▀▀██     ▀▀
+      ██      ██       ▄█████▄ ██▄███▄  ██ ▀██  ███     ▄█████▄ ██   ████
+     ██        ██      ▀ ▄▄▄██ ██▀  ▀██ ██  ██▄ ██     ██▀    ▀ ██     ██
+    ██   ████   ██    ▄██▀▀▀██ ██    ██ ██   ████▀ ███ ██       ██     ██
+   ██  ███  ███  ██   ██▄▄▄███ ███▄▄██▀ ██▄▄▄ ███      ▀██▄▄▄▄█ ██▄▄ ▄▄██▄
+  █████        █████   ▀▀▀▀ ▀▀ ▀▀ ▀▀▀    ▀▀▀▀ ██         ▀▀▀▀▀   ▀▀▀ ▀▀▀▀▀
+ ███              ███                       ███
+
+ We're looking for great people to join us. See https://ably.com/careers
+
+ ###  Keyboard shortcuts
+
+ - a  Attach to a channel
+ - d  Detach from channel
+ - m  Publish a message
+ - r  Replay last received message
+ - n  Set message event name
+ - h  Toggle heartbeat message
+ - e  Enter presence
+ - l  Leave presence
+ - ?  This help message
+ - q  Quit
+
+`
+    return ascii;
+}
